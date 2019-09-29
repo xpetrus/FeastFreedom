c6c649f2144aa1cdbb37a10381276faa94e41109
xpetrus <phsiao24@gmail.com>
2019-09-28 22:12:14 -0400

    kitchen cards

--- FeastFreedom_Angular/FeastFreedom/src/app/kitchen/kitchen.component.ts
+++ FeastFreedom_Angular/FeastFreedom/src/app/kitchen/kitchen.component.ts
@@ -10,6 +10,8 @@ import { Router } from '@angular/router';
 })
 export class KitchenComponent implements OnInit {
 
+  tempKitchens = ['Burger King', 'Wendys', 'White Castle'];
+
   constructor(private userService: UserService, private cookieService: CookieService, private router: Router) { }
 
   ngOnInit() {
